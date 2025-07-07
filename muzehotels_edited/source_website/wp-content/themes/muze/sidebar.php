7044102aac8a06186144c68db7e99f4e6e5838d7
googprole <googprole@gmail.com>
2025-07-07 17:27:31 +0700

    Delete muzehotels_edited directory

--- muzehotels_edited/source_website/wp-content/themes/muze/sidebar.php
+++
@@ -1,5 +0,0 @@
-<div class="col-sm-3">
-	<div class="sidebar">
-		<?php dynamic_sidebar("Sidebar"); ?>
-	</div>
-</div>
--- muzehotels_edited/source_website/wp-content/themes/muze/template/contruction.php
+++
@@ -1,8 +0,0 @@
-  
-<?php
-/**
- * Template Name: Contruction Template
- */
-?>
-<div class="white-space space-big" style="padding:100px;"></div>
-<h1 style="text-align: center;">UNDER CONTRUCTION</h1>
